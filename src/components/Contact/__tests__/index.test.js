d45990dc06ddff70d14b5b61aec0229ab09d994c
Ridercogswell <ridercogswell@gmail.com>
2022-05-14 13:46:50 -0700

    add tests:

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '../../Contact';
+
+describe('contact component', () => {
+    it('renders', () => {
+        <Contact/>
+    })
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Contact />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
+
+it('renders', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+
+it('render', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('button')).toHaveTextContent('Submit')
+})
